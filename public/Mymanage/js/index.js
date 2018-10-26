2f89fadcf83d1bd34834537638c5c9e239307776
wenhellowold <914926182@qq.com>
2018-10-26 08:52:48 +0800

    首页完成

---
+++ public/Mymanage/js/index.js
@@ -0,0 +1,69 @@
+;(function(){
+  var Chart_left = echarts.init(document.querySelector('.echarts_left'));
+
+  // 指定图表的配置项和数据
+  var option_left = {
+      title: {
+          text: '2018年注册人数'
+      },
+      tooltip: {},
+      legend: {
+          data:['人数']
+      },
+      xAxis: {
+          data: ["1月","2月","3月","4月","5月","6月"]
+      },
+      yAxis: {},
+      series: [{
+          name: '人数',
+          type: 'bar',
+          data: [500, 1500, 1000, 2000, 2500,2200]
+      }]
+  };
+
+  // 使用刚指定的配置项和数据显示图表。
+  Chart_left.setOption(option_left)
+
+  var Chart_right = echarts.init(document.querySelector('.echarts_right'));
+  option_right = {
+    title : {
+        text: '热门品牌销售',
+        subtext: '2018年7月',
+        x:'center'
+    },
+    tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        orient: 'vertical',
+        left: 'left',
+        data: ['耐克','李宁','新百伦','鸿星尔克','阿迪王']
+    },
+    series : [
+        {
+            name: '访问来源',
+            type: 'pie',
+            radius : '55%',
+            center: ['50%', '60%'],
+            data:[
+                {value:335, name:'耐克'},
+                {value:310, name:'李宁'},
+                {value:234, name:'新百伦'},
+                {value:135, name:'鸿星尔克'},
+                {value:1548, name:'阿迪王'}
+            ],
+            itemStyle: {
+                emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+        }
+    ]
+}
+Chart_right.setOption(option_right)
+
+
+})()
